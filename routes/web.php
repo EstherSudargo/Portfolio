35f0b6be617651848739de16b7f70c1393acb427
Esther Regina Sudargo <etsu@esthers-mac.local>
2026-06-04 23:22:14 +0800

    fix route

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/Research', function () {
-    return view('research');
+    return view('home');
 });
 
 Route::get('/home', function () {
     return view('home');
-});
-Route::get('/aboutme', function(){
-    return view('aboutme');
-});
-Route::get('/figma', function(){
-    return view('figma');
-});
-Route::get('/data_based_projects', function(){
-    return view('data_based_projects');
 });
